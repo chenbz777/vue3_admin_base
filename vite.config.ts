f532859bdc3cb03d328dda05d9623aaf9ed5ee51
chenbz777 <321146822@qq.com>
2022-12-05 11:15:09 +0800

    新增请求代理

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,18 @@ export default defineConfig({
     }),
   ],
   base: './',
+  server: { //主要是加上这段代码
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      // 字符串简写写法
+      '/foo': 'http://localhost:4567',
+      // 选项写法
+      '/api': {
+        target: 'http://jsonplaceholder.typicode.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      },
+    }
+  },
 })
